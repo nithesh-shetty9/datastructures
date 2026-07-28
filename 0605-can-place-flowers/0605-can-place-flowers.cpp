9411b16adc5c18d0ec02616b940379989d5de4c3
Nithesh shetty <nitheshnithesh310@gmail.com>
2026-07-28 12:32:41 +0530

    Time: 0 ms (100%), Space: 24.2 MB (45.16%) - LeetHub

---
+++ 0605-can-place-flowers/0605-can-place-flowers.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    bool canPlaceFlowers(vector<int>&nums, int n) {
+        int size=nums.size();
+        if(n==0)return true;
+        for(int i=0;i<size;i++)
+        {
+            if(nums.size()==1)
+            {
+                if(nums[i]==0)
+                {
+                n--;
+                nums[i]=1;
+                }
+            }
+            else if(i==0&&nums[i]==0&&nums[i+1]==0)
+            {
+                n--;
+                nums[i]=1;
+            }
+            else if(i==size-1&&nums[i-1]==0&&nums[i]==0)
+            {
+                n--;
+                nums[i]=1;
+            }
+            else if(i>0&&i<size-1&&nums[i]==0&&nums[i-1]==0&&nums[i+1]==0)
+            {
+                n--;
+                nums[i]=1;
+            }
+            
+           if(n==0)
+           {
+            return true;
+           }
+        }
+        return false;
+    }
+};
