7e05e4eae191002afc65aa589741f613f48ce38d
Nithesh shetty <nitheshnithesh310@gmail.com>
2026-08-03 13:17:39 +0530

    Time: 0 ms (100%), Space: 8.1 MB (75.16%) - LeetHub

---
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool checkValidString(string s) {
+      int open=0;
+      int close=0;
+      int n=s.size();
+      for(int i=0;i<n;i++)
+      {
+        if(s[i]=='('||s[i]=='*')
+        {
+            open++;
+        }
+        else
+        {
+            open--;
+        }
+        if(s[n-i-1]==')'||s[n-i-1]=='*')
+        {
+            close++;
+        }
+        else
+        {
+            close--;
+        }
+        if(open<0||close<0)
+        {
+            return false;
+        }
+      }
+      return true;  
+    }
+};
