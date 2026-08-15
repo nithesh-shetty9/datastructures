3702afc790e0bb63bfe0437f0142f556fc8d3e15
Nithesh shetty <nitheshnithesh310@gmail.com>
2026-08-15 20:11:35 +0530

    Time: 4 ms (65.48%), Space: 15.3 MB (69.95%) - LeetHub

--- 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.cpp
+++ 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.cpp
@@ -1,16 +1,16 @@
 /**
- * Definition for a binary tree TreeNode  .
- * struct TreeTreeNode {
+ * Definition for a binary tree node.
+ * struct TreeNode {
  *     int val;
- *     TreeTreeNod *left;
- *     TreeTreeNod *right;
- *     TreeTreeNod(int x) : val(x), left(NULL), right(NULL) {}
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
 class Solution {
 public:
     vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
-       unordered_map<TreeNode *,TreeNode*>mpp;
+        unordered_map<TreeNode*,TreeNode*>mpp;
         queue<TreeNode*>q;
         q.push(root);
         while(!q.empty())
@@ -18,56 +18,55 @@ public:
             int n=q.size();
             for(int i=0;i<n;i++)
             {
-                TreeNode*temp=q.front();
+                root=q.front();
                 q.pop();
-                if(temp->left)
+                if(root->left)
                 {
-                    q.push(temp->left);
-                    mpp[temp->left]=temp;
+                    q.push(root->left);
+                    mpp[root->left]=root;
                 }
-                 if(temp->right)
+                if(root->right)
                 {
-                    q.push(temp->right);
-                    mpp[temp->right]=temp;
+                    q.push(root->right);
+                    mpp[root->right]=root;
                 }
-            }
+            }  
         }
-        queue<TreeNode*>final;
+        vector<int>ans;
+        queue<TreeNode*>track;
+        track.push(target);
         unordered_map<TreeNode*,bool>visited;
-        visited[target] = true;
-        final.push(target);
+        visited[target]=true;
         while(k--)
         {
-            int n=final.size();
+            int n=track.size();
             for(int i=0;i<n;i++)
             {
-            root=final.front();
-            final.pop();
-            if(root->left&&!visited[root->left])
-            {
-                final.push(root->left);
-                visited[root->left]=true;
-            }
-            if(root->right&&!visited[root->right])
-            {
-                final.push(root->right);
-                visited[root->right]=true;
-            }
-            if(mpp[root]&&!visited[mpp[root]])
-            {
-                final.push(mpp[root]);
-                visited[mpp[root]]=true;
-            }
+                root=track.front();
+                track.pop();
+                if(root->left&&!visited[root->left])
+                {
+                    visited[root->left]=true;
+                    track.push(root->left);
+                }
+                if(root->right&&!visited[root->right])
+                {
+                    visited[root->right]=true;
+                    track.push(root->right);
+                }
+                if(mpp[root]&&!visited[mpp[root]])
+                {
+                    visited[mpp[root]]=true;
+                    track.push(mpp[root]);
+                }
             }
         }
-        vector<int>res;
-        while(!final.empty())
+        while(!track.empty())
         {
-            res.push_back(final.front()->val);
-            final.pop();
+            ans.push_back(track.front()->val);
+            track.pop();
         }
-        sort(res.begin(),res.end());
-        return res;
+        return ans;
+
     }
-   
 };
