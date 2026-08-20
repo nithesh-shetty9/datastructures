e83bfd1469780ae29ef07c048a3eada9f003a88b
Nithesh shetty <nitheshnithesh310@gmail.com>
2026-08-20 17:26:54 +0530

    Time: 0 ms (100%), Space: 19.6 MB (95.72%) - LeetHub

--- 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.cpp
+++ 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.cpp
@@ -1,37 +1,37 @@
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
- *     long long val;
+ *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(long long x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(long long x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-    long long widthOfBinaryTree(TreeNode* root) {
+    int widthOfBinaryTree(TreeNode* root) {
+        long long maxi=0;
         queue<pair<TreeNode*,long long>>q;
-        long long width=0;
         q.push({root,0});
         while(!q.empty())
         {
-            long long n=q.size();
-            long long mini=q.front().second;
+            int n=q.size();
             long long first,last;
-            for(long long i=0;i<n;i++)
+            long long mini=q.front().second;
+            for(int i=0;i<n;i++)
             {
-                long long index=q.front().second-mini;
                 root=q.front().first;
+                int idx=q.front().second-mini;
                 q.pop();
-                if(i==0)first=index;
-                if(i==n-1)last=index;
-                if(root->left)q.push({root->left,index*2+1});
-                if(root->right)q.push({root->right,index*2+2});
+                if(i==0)first=idx;
+                if(i==n-1)last=idx;
+                if(root->left)q.push({root->left,idx*2LL+1});
+                if(root->right)q.push({root->right,idx*2LL+2});
             }
-            width=max(width,last-first+1);
+            maxi=max(maxi,first+last+1);
         }
-        return width;
+        return maxi;
     }
 };
