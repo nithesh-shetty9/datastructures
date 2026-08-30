5425def254d33eda66638f9e835b11820249aa62
Nithesh shetty <nitheshnithesh310@gmail.com>
2026-08-30 14:12:30 +0530

    Time: 2 ms (41.13%), Space: 20.7 MB (28.13%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,30 +1,40 @@
 class Solution {
 public:
     int findCircleNum(vector<vector<int>>& isConnected) {
-        int n = isConnected.size();
+        int V=isConnected.size();
+        vector<vector<int>>adj(V+1);
+        vector<int>visited(V+1,0);
+        int count=0;
+        for(int i=0;i<V;i++)
+        {
+            for(int j=0;j<V;j++)
+            {
+                if(isConnected[i][j]==1)
+                {
+               adj[i+1].push_back(j+1);
+                }
 
-        vector<bool> visited(n, false);
-        int provinces = 0;
-
-        for(int i = 0; i < n; i++) {
-            if(!visited[i]) {
-                provinces++;
-                bfs(i, isConnected, visited);
             }
         }
-
-        return provinces;
+        for(int i=1;i<=V;i++)
+        {
+            if(!visited[i])
+            {
+                count++;
+                depth(adj,i,visited);
+            }
+        }
+        return count;
     }
-
-    void bfs(int node, vector<vector<int>>& isConnected,
-             vector<bool>& visited) {
-                visited[node]=true;
-                for(int i=0;i<isConnected.size();i++)
-                {
-                    if(!visited[i]&&isConnected[node][i]==1)
-                    {
-                        bfs(i,isConnected,visited);
-                    }
-                }
+    void depth(vector<vector<int>>&adj,int node,vector<int>&visited)
+    {
+        visited[node]=1;
+       for(auto it:adj[node])
+       {
+           if(!visited[it])
+           {
+               depth(adj,it,visited);
+           }
+       }
     }
 };
